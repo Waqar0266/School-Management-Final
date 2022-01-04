9f72274c2c1ffd703eda288c6db9706de13dc483
Asad Ullah Arif <asadarif91@hotmail.com>
2022-01-05 00:57:52 +0500

    asad added

---
+++ School_Management_System/ViewModel/AssetVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Management_System.ViewModel
+{
+    class AssetVM
+    {
+        public int as_id { get; set; }
+        public string type_ { get; set; }
+        public string name_ { get; set; }
+        public Nullable<System.DateTime> date_ { get; set; }
+        public Nullable<decimal> amount { get; set; }
+        public string descripition { get; set; }
+    }
+}
