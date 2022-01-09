41b920ab465e0a87cc99f21dfff1495057e979b9
Asad Ullah Arif <asadarif91@hotmail.com>
2022-01-09 17:29:08 +0500

    asad added

---
+++ School_Management_System/Salary/Edit_Salary.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace School_Management_System.Salary
+{
+    partial class Edit_Salary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblsalary = new System.Windows.Forms.Label();
+            this.EditSave = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.EditSalaryTotal = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.EditSalaryAdvance = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.EditSalaryAllowances = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.EditSalaryDedcution = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.EditSalaryBasic = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EditempDesgnText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EditsalaryIssueDate = new System.Windows.Forms.DateTimePicker();
+            this.EditempCnicText = new System.Windows.Forms.TextBox();
+            this.EditSalaryMonth = new System.Windows.Forms.DateTimePicker();
+            this.EditempNameText = new System.Windows.Forms.TextBox();
+            this.EditSalaryEmpID = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblsalary
+            // 
+            this.lblsalary.AutoSize = true;
+            this.lblsalary.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsalary.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(163)))));
+            this.lblsalary.Location = new System.Drawing.Point(129, 6);
+            this.lblsalary.Name = "lblsalary";
+            this.lblsalary.Size = new System.Drawing.Size(117, 25);
+            this.lblsalary.TabIndex = 36;
+            this.lblsalary.Text = "Edit Salary";
+            // 
+            // EditSave
+            // 
+            this.EditSave.BackColor = System.Drawing.Color.SteelBlue;
+            this.EditSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.EditSave.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EditSave.ForeColor = System.Drawing.Color.White;
+            this.EditSave.Location = new System.Drawing.Point(85, 547);
+            this.EditSave.Name = "EditSave";
+            this.EditSave.Size = new System.Drawing.Size(200, 52);
+            this.EditSave.TabIndex = 35;
+            this.EditSave.Text = "Save";
+            this.EditSave.UseVisualStyleBackColor = false;
+            this.EditSave.Click += new System.EventHandler(this.EditSave_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(163)))));
+            this.label11.Location = new System.Drawing.Point(18, 492);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(121, 20);
+            this.label11.TabIndex = 33;
+            this.label11.Text = "Total Amount:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(163)))));
+            this.label10.Location = new System.Drawing.Point(6, 448);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(138, 20);
+            this.label10.TabIndex = 32;
+            this.label10.Text = "Advance Salary:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(163)))));
+            this.label9.Location = new System.Drawing.Point(34, 404);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(104, 20);
+            this.label9.TabIndex = 31;
+            this.label9.Text = "Allowances:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(163)))));
+            this.label8.Location = new System.Drawing.Point(42, 360);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 20);
+            this.label8.TabIndex = 30;
+            this.label8.Text = "Deduction:";
+            // 
+            // EditSalaryTotal
+            // 
+            this.EditSalaryTotal.Location = new System.Drawing.Point(145, 491);
+            this.EditSalaryTotal.Name = "EditSalaryTotal";
+            this.EditSalaryTotal.Size = new System.Drawing.Size(200, 20);
+            this.EditSalaryTotal.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(163)))));
+            this.label7.Location = new System.Drawing.Point(25, 316);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 20);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Basic Salary:";
+            // 
+            // EditSalaryAdvance
+            // 
+            this.EditSalaryAdvance.Location = new System.Drawing.Point(145, 447);
+            this.EditSalaryAdvance.Name = "EditSalaryAdvance";
+            this.EditSalaryAdvance.Size = new System.Drawing.Size(200, 20);
+            this.EditSalaryAdvance.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(163)))));
+            this.label6.Location = new System.Drawing.Point(28, 272);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 20);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Issue Date:";
+            // 
+            // EditSalaryAllowances
+            // 
+            this.EditSalaryAllowances.Location = new System.Drawing.Point(144, 403);
+            this.EditSalaryAllowances.Name = "EditSalaryAllowances";
+            this.EditSalaryAllowances.Size = new System.Drawing.Size(200, 20);
+            this.EditSalaryAllowances.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(163)))));
+            this.label5.Location = new System.Drawing.Point(66, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Month:";
+            // 
+            // EditSalaryDedcution
+            // 
+            this.EditSalaryDedcution.Location = new System.Drawing.Point(144, 359);
+            this.EditSalaryDedcution.Name = "EditSalaryDedcution";
+            this.EditSalaryDedcution.Size = new System.Drawing.Size(200, 20);
+            this.EditSalaryDedcution.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(163)))));
+            this.label4.Location = new System.Drawing.Point(25, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 20);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "Designation:";
+            // 
+            // EditSalaryBasic
+            // 
+            this.EditSalaryBasic.Location = new System.Drawing.Point(144, 315);
+            this.EditSalaryBasic.Name = "EditSalaryBasic";
+            this.EditSalaryBasic.Size = new System.Drawing.Size(200, 20);
+            this.EditSalaryBasic.TabIndex = 22;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(163)))));
+            this.label3.Location = new System.Drawing.Point(81, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Cnic:";
+            // 
+            // EditempDesgnText
+            // 
+            this.EditempDesgnText.Location = new System.Drawing.Point(144, 183);
+            this.EditempDesgnText.Name = "EditempDesgnText";
+            this.EditempDesgnText.ReadOnly = true;
+            this.EditempDesgnText.Size = new System.Drawing.Size(200, 20);
+            this.EditempDesgnText.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(163)))));
+            this.label2.Location = new System.Drawing.Point(66, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Name:";
+            // 
+            // EditsalaryIssueDate
+            // 
+            this.EditsalaryIssueDate.Location = new System.Drawing.Point(144, 271);
+            this.EditsalaryIssueDate.Name = "EditsalaryIssueDate";
+            this.EditsalaryIssueDate.Size = new System.Drawing.Size(200, 20);
+            this.EditsalaryIssueDate.TabIndex = 24;
+            // 
+            // EditempCnicText
+            // 
+            this.EditempCnicText.Location = new System.Drawing.Point(144, 139);
+            this.EditempCnicText.Name = "EditempCnicText";
+            this.EditempCnicText.ReadOnly = true;
+            this.EditempCnicText.Size = new System.Drawing.Size(200, 20);
+            this.EditempCnicText.TabIndex = 16;
+            // 
+            // EditSalaryMonth
+            // 
+            this.EditSalaryMonth.Location = new System.Drawing.Point(144, 227);
+            this.EditSalaryMonth.Name = "EditSalaryMonth";
+            this.EditSalaryMonth.Size = new System.Drawing.Size(200, 20);
+            this.EditSalaryMonth.TabIndex = 23;
+            // 
+            // EditempNameText
+            // 
+            this.EditempNameText.Location = new System.Drawing.Point(144, 95);
+            this.EditempNameText.Name = "EditempNameText";
+            this.EditempNameText.ReadOnly = true;
+            this.EditempNameText.Size = new System.Drawing.Size(200, 20);
+            this.EditempNameText.TabIndex = 15;
+            // 
+            // EditSalaryEmpID
+            // 
+            this.EditSalaryEmpID.FormattingEnabled = true;
+            this.EditSalaryEmpID.Location = new System.Drawing.Point(144, 49);
+            this.EditSalaryEmpID.Name = "EditSalaryEmpID";
+            this.EditSalaryEmpID.Size = new System.Drawing.Size(200, 21);
+            this.EditSalaryEmpID.TabIndex = 14;
+            this.EditSalaryEmpID.SelectedIndexChanged += new System.EventHandler(this.EditSalaryEmpID_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(163)))));
+            this.label1.Location = new System.Drawing.Point(52, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Emp ID:";
+            // 
+            // Edit_Salary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 618);
+            this.Controls.Add(this.lblsalary);
+            this.Controls.Add(this.EditSave);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.EditSalaryTotal);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.EditSalaryAdvance);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.EditSalaryAllowances);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.EditSalaryDedcution);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.EditSalaryBasic);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.EditempDesgnText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.EditsalaryIssueDate);
+            this.Controls.Add(this.EditempCnicText);
+            this.Controls.Add(this.EditSalaryMonth);
+            this.Controls.Add(this.EditempNameText);
+            this.Controls.Add(this.EditSalaryEmpID);
+            this.Controls.Add(this.label1);
+            this.Name = "Edit_Salary";
+            this.Text = "Edit_Salary";
+            this.Load += new System.EventHandler(this.Edit_Salary_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblsalary;
+        private System.Windows.Forms.Button EditSave;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox EditSalaryTotal;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox EditSalaryAdvance;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox EditSalaryAllowances;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox EditSalaryDedcution;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox EditSalaryBasic;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox EditempDesgnText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker EditsalaryIssueDate;
+        private System.Windows.Forms.TextBox EditempCnicText;
+        private System.Windows.Forms.DateTimePicker EditSalaryMonth;
+        private System.Windows.Forms.TextBox EditempNameText;
+        private System.Windows.Forms.ComboBox EditSalaryEmpID;
+        private System.Windows.Forms.Label label1;
+    }
+}
